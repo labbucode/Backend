e8c98a46d58813f923e2deb210cb9ad829765273
Laraib <syedlaraib745@gmail.com>
2023-10-26 19:44:55 +0530

    delay and chart

--- router/listingsRouter.js
+++ router/listingsRouter.js
@@ -70,6 +70,61 @@ router.get('/stats', async (req, res) => {
     }
 });
 
+router.get('/department-status-stats', async (req, res) => {
+    try {
+      const departmentStats = await lisitngModel.aggregate([
+        {
+          $group: {
+            _id: "$department",
+            statusCounts: {
+              $push: { status: "$status", count: 1 }
+            }
+          }
+        },
+        {
+          $project: {
+            department: "$_id",
+            statusCounts: 1,
+            _id: 0
+          }
+        }
+      ]);
+  
+      const stats = {};
+  
+      departmentStats.forEach(department => {
+        const { department: departmentName, statusCounts } = department;
+  
+        const departmentStatusCounts = {
+            Running: 0,
+            Cancelled: 0, 
+            Closed: 0, 
+            Registered: 0,
+        };
+
+       
+  
+        statusCounts.forEach(statusCount => {
+          const { status, count } = statusCount;
+          if (departmentStatusCounts[status]) {
+            departmentStatusCounts[status] += count;
+          } else {
+            departmentStatusCounts[status] = count;
+          }
+        });
+  
+        stats[departmentName] = departmentStatusCounts;
+      });
+  
+      res.json(stats);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Server error' });
+    }
+  });
+  
+  
+
 
 router.post('/', async (req, res) => {
     const { project_name,
